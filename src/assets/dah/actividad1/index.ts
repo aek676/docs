db36129d11d4fd93e962b14c1e76d1c632219307
aek676 <aek676@inlumine.ual.es>
2026-03-08 12:53:26 +0100

    feat(dah): add actividad1 assets and docs update
    Added new image assets for actividad1 (Login, Clients, Reservation,
    Products)
    and updated the actividad1 documentation to include visual previews,
    improved
    explanations of navigation, login flow, and custom theme integration.
    Also
    added instructions for running the app and mapped theme variables to
    Ionic.

---
+++ src/assets/dah/actividad1/index.ts
@@ -0,0 +1,6 @@
+import LoginImg from './Login.png';
+import ClientsImg from './Clients.png';
+import ReservationImg from './Reservation.png';
+import ProductsImg from './Products.png';
+
+export { LoginImg, ClientsImg, ReservationImg, ProductsImg };
