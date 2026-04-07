114fabdbaea122c754a9d9a6a1830e4566027dba
aek676 <aek676@inlumine.ual.es>
2026-04-07 23:00:33 +0200

    feat(docs): add actividad 4 guide and screenshots
    Add detailed documentation for "Actividad 4" covering the creation of an
    Ionic app with Firebase Authentication and Cloud Firestore integration.
    Includes step-by-step setup, code examples, and answers to key
    questions.
    Add related screenshots and an image index for visual reference.

---
+++ src/assets/dah/actividad4/img/index.ts
@@ -0,0 +1,8 @@
+import HomeScreen from './Home.png';
+import LoginScreen from './SignIn.png';
+import ProductsScreen from './Products.png';
+import AddProductScreen from './AddProduct.png';
+import PublicListScreen from './ProductList.png';
+import ProductDetailScreen from './ProductDetails.png';
+
+export { HomeScreen, LoginScreen, ProductsScreen, AddProductScreen, PublicListScreen, ProductDetailScreen };
