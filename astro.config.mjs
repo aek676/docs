f3c9297009d8bac49bddbd0452f84cf756917344
aek676 <aek676@inlumine.ual.es>
2026-04-22 14:14:18 +0200

    feat(deps): upgrade Astro & Starlight, add astro-d2
    Upgraded Astro to v6 and @astrojs/starlight to v0.38.3 for improved
    features and compatibility. Added astro-d2 and @terrastruct/d2 for D2
    diagram support. Updated related dependencies and configuration in
    astro.config.mjs and package.json. Also switched to zod v4.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,40 +1,45 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 import { starlightKatex } from "starlight-katex";
+
+import d2 from "astro-d2";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://aek676.github.io',
-  base: '/docs',
+  site: "https://aek676.github.io",
+  base: "/docs",
   integrations: [
-    starlight(
-      {
-        plugins: [starlightKatex()],
-        title: 'Anass',
-        social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/aek676' }],
-        sidebar: [
-          {
-            label: 'Inicio',
-            items: [
-              { label: 'Introducción', slug: 'home/introduction' },
-            ],
-          },
-          {
-            label: 'Máster',
-            items: [
-              { label: 'Índice', slug: 'master' },
-              {
-                label: 'Desarrollo de Aplicaciones Híbridas',
-                autogenerate: { directory: 'master/dah' },
-              },
-              {
-                label: 'Desarrollo web basado en servicios y componentes',
-                autogenerate: { directory: 'master/dwsc' },
-              }
-            ]
-          }
-        ],
-      }),
+    starlight({
+      plugins: [starlightKatex()],
+      title: "Anass",
+      social: [
+        { icon: "github", label: "GitHub", href: "https://github.com/aek676" },
+      ],
+      sidebar: [
+        {
+          label: "Inicio",
+          items: [{ label: "Introducción", slug: "home/introduction" }],
+        },
+        {
+          label: "Máster",
+          items: [
+            { label: "Índice", slug: "master" },
+            {
+              label: "Desarrollo de Aplicaciones Híbridas",
+              autogenerate: { directory: "master/dah" },
+            },
+            {
+              label: "Desarrollo web basado en servicios y componentes",
+              autogenerate: { directory: "master/dwsc" },
+            },
+          ],
+        },
+      ],
+    }),
+    d2({
+      experimental: { useD2js: true }
+    }),
   ],
 });
+
